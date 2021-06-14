ab3b1538604ab461988d58108a4e5f054a8136c0
ForzBox7 <salmananaqvii+github@gmail.com>
2021-06-14 20:27:13 +0500

    Added the intro message.

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,5 +1,10 @@
 //  Created by Salman Naqvi on 14/06/2021.
 
+#include <iostream>
+
 int main() {
+    std::cout << "You are an agent attempting to disarm a nuclear weapon." << std::endl;
+    std::cout << std::endl << "You must input the correct codes to prevent the weapon from activating." << std::endl;
+    
     return 0;
 }
