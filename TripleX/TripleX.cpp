8a14dd3d28a0d67a04b7c00502194b1d4ce5c76d
ForzBox7 <salmananaqvii+github@gmail.com>
2021-06-15 16:52:22 +0500

    Created variables to store the three number code, code sum, code product, and player guess.

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -3,8 +3,24 @@
 #include <iostream>
 
 int main() {
+    
+    // Print welcome messages to the terminal.
     std::cout << "You are an agent attempting to disarm a nuclear weapon." << std::endl;
-    std::cout << std::endl << "You must input the correct codes to prevent the weapon from activating." << std::endl;
+    std::cout << std::endl << "You must input the correct 3 number code to prevent the weapon from activating." << std::endl;
+    
+    // Declare three number code.
+    const int CodeA = 7;
+    const int CodeB = 6;
+    const int CodeC = 5;
+    
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+    
+    // Print CodeSum and CodeProduct to the terminal.
+    std::cout << std::endl << "The sum of the three numbers is " << CodeSum << "." << std::endl;
+    std::cout << "The product of the three numbers is " << CodeProduct << "." << std::endl;
+    
+    int PlayerGuess;
     
     return 0;
 }
