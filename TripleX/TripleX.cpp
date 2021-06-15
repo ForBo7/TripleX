eb78c089b5bc2bc3e8d2c85ec89607b2539c83f6
ForzBox7 <salmananaqvii+github@gmail.com>
2021-06-15 21:23:26 +0500

    Encapsulated parts of the code into functions.

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -2,11 +2,41 @@
 
 #include <iostream>
 
-int main() {
-    
+void PrintIntroduction() {
     // Print welcome messages to the terminal.
-    std::cout << "You are an agent attempting to disarm a nuclear weapon." << std::endl;
-    std::cout << std::endl << "You must input the correct 3 number code to prevent the weapon from activating." << std::endl;
+    std::cout << R"(
+    WWWWWWWWWWWWWWWWWWWNX00OOOO00KNWWWWWWWWWWWWWWWWWWW
+    WWWWWWWWWWWWWWKkoc;'..''''....',cokKWWWWWWWWWWWWWW
+    WWWWWWWWWWNOo;..,:oxk00KKKK00kxo:,..;o0NWWWWWWWWWW
+    WWWWWWWWXd;..:x0NWWWWWWWWWWWWWWWWNKx:..,xXWWWWWWWW
+    WWWWWWXd'.,xXWK0NWWWWWWWWWWWWWWWWWK0NXd,.'dXWWWWWW
+    WWWWWO;.'xNNOc..oXWWWWWWWWWWWWWWWk'.;xXNx' ;0WWWWW
+    WWWWx..lXW0:.    cXWWWWWWWWWWWWNd.    ,kNXc..xWWWW
+    WWWx..dNWx.       ;KWWWWWWWWWWNo.       cXNd..xWWW
+    WWO. oNWd.         'OWWWWWWWWXc          :XNo .OWW
+    WNc ;XMk.           .kWWWWWWK:            lNX; cNW
+    W0'.dWN:            ,OWX00KNXl.           'OWd..OW
+    Wk..OMK;...........:KNd'...:KWd............xMO..xW
+    Wx..OMNK00000000000XWX:    .kMNK00000000000XWO..xW
+    Wk..xWWWWWWWWWWWWWWWWWKdlloONWWWWWWWWWWWWWWWWx..kW
+    WX; cNWWWWWWWWWWWWWWWXkxkOkx0WWWWWWWWWWWWWWWNc ;KW
+    WWx..kWWWWWWWWWWWWWWK:      .kWWWWWWWWWWWWWWk..xWW
+    WWNl 'OWWWWWWWWWWWW0,        .xNWWWWWWWWWWWO' lNWW
+    WWWXc 'kWWWWWWWWWWO'          .oNWWWWWWWWWk' cXWWW
+    WWWWNo..lKWWWWWWWx.             cXWWWWWWXl..oXWWWW
+    WWWWWWO;..oKWWWWx.               lXWWWKd'.;OWWWWWW
+    WWWWWWWNk;..ckXWKxl:,........';ld0NXkc..;kNWWWWWWW
+    WWWWWWWWWNOl,..;ox0XXXXKKKKKXXX0ko;..,lONWWWWWWWWW
+    WWWWWWWWWWWWN0dc,...',;::::;,'...,cd0NWWWWWWWWWWWW
+    WWWWWWWWWWWWWWWWNX0kdoolllloodk0XNWWWWWWWWWWWWWWWW
+    WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+    WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW)" << std::endl << std::endl;
+    std::cout << "You are an agent attempting to disarm a nuclear weapon.\n";
+    std::cout << "\nYou must input the correct 3 number code to prevent the weapon from activating.\n";
+}
+
+void PlayGame() {
+    PrintIntroduction();
     
     // Declare three number code.
     const int CodeA = 7;
@@ -17,24 +47,28 @@ int main() {
     const int CodeProduct = CodeA * CodeB * CodeC;
     
     // Print CodeSum and CodeProduct to the terminal.
-    std::cout << std::endl << "The sum of the three numbers is " << CodeSum << "." << std::endl;
-    std::cout << "The product of the three numbers is " << CodeProduct << "." << std::endl;
+    std::cout << std::endl << "\nThe sum of the three numbers is " << CodeSum << ".\n";
+    std::cout << "The product of the three numbers is " << CodeProduct << ".\n";
     
-    int GuessA;
-    int GuessB;
-    int GuessC;
-    std::cout << std::endl << "Enter your guess: ";
+    // Store player's guess.
+    int GuessA, GuessB, GuessC;
+    std::cout << "\nEnter your guess: ";
     std::cin >> GuessA >> GuessB >> GuessC;
     std::cout << "You entered: " << GuessA << GuessB << GuessC << std::endl;
     
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
     
+    // Check if they player's guess is correct.
     if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
-        std::cout << std::endl << "You've disarmed the weapon and saved the day!" << std::endl;
+        std::cout << "\nYou've disarmed the weapon and saved the day!\n";
     } else {
-        std::cout << std:: endl << "And the weapon activates..." << std::endl;
+        std::cout << "\nAnd the weapon activates...\n";
     }
+}
+
+int main() {
+    PlayGame();
     
     return 0;
 }
